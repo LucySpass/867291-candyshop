8b77f1f1eb7c30574920328635fc5cab2931700e
ivana.begovic@yahoo.com <ivana.begovic@yahoo.com>
2018-10-07 22:54:49 +0300

    fixes

--- js/products.js
+++ js/products.js
@@ -399,91 +399,28 @@ var productModule = (function () {
 })();
 
 var filterModule = (function () {
-  var rangeMin = document.querySelector('.range__btn--left');
-  var rangeMax = document.querySelector('.range__btn--right');
-  var priceMin = document.querySelector('.range__price--min');
-  var priceMax = document.querySelector('.range__price--max');
-  var sliderLine = document.querySelector('.range__filter');
-  var sliderFillLine = document.querySelector('.range__fill-line');
-  // var filterCallback;
+  var rangeBtnLeft = document.querySelector('.range__btn--left');
+  var rangeBtnRight = document.querySelector('.range__btn--right');
+  var maxPriceElement = document.querySelector('.range__price--max');
+  var minPriceElement = document.querySelector('.range__price--min');
+  var filterCallback;
 
-  var min = parseInt(getComputedStyle(rangeMin).left, 10);
-  var max = parseInt(getComputedStyle(rangeMax).left, 10);
-  var MIN = 0;
-  var MAX = 245;
-  var ELEMENT_WIDTH = 240;
-  var sliderLineCoords = getCoords(sliderLine);
-
-  function rangeMaxMouseDownHandler(evt) {
-    var elMaxCoords = getCoords(rangeMax);
-    var shiftX = evt.pageX - elMaxCoords.left;
-    document.addEventListener('mousemove', rangeMaxMouseMoveHandler);
-
-    function rangeMaxMouseMoveHandler(e) {
-      priceMax.textContent = parseInt(max, 10);
-
-      var newRight = e.pageX - shiftX - sliderLineCoords.left;
-      if (newRight > MAX) {
-        newRight = MAX;
-      }
-      if (newRight < min + rangeMin.offsetWidth / 2) {
-        newRight = min + rangeMin.offsetWidth / 2;
-      }
-      max = newRight;
-      rangeMax.style.left = newRight + 'px';
-      sliderFillLine.style.right = ELEMENT_WIDTH - newRight + 'px';
+  function changePriceRange(event) {
+    if (event.target.innerText === 'правый ползунок') {
+      maxPriceElement.innerHTML = 'lala';
+    } else {
+      minPriceElement.innerHTML = 'la';
     }
-
-    function rangeMaxMouseUpHandler() {
-      document.removeEventListener('mousemove', rangeMaxMouseMoveHandler);
-      document.removeEventListener('mouseup', rangeMaxMouseUpHandler);
-    }
-
-    document.addEventListener('mouseup', rangeMaxMouseUpHandler);
-  }
-
-  function rangeMinMouseDownHandler(evt) {
-    var elMinCoords = getCoords(rangeMin);
-    var shiftX = evt.pageX - elMinCoords.left;
-    document.addEventListener('mousemove', rangeMinMouseMoveHandler);
-
-    function rangeMinMouseMoveHandler(e) {
-      priceMin.textContent = parseInt(min, 10);
-      var newLeft = e.pageX - shiftX - sliderLineCoords.left;
-      if (newLeft < MIN) {
-        newLeft = MIN;
-      }
-      if (newLeft > max - rangeMax.offsetWidth / 2) {
-        newLeft = max - rangeMax.offsetWidth / 2;
-      }
-      min = newLeft;
-      rangeMin.style.left = newLeft + 'px';
-      sliderFillLine.style.left = newLeft + 'px';
-    }
-
-    function rangeMinMouseUpHandler() {
-      document.removeEventListener('mousemove', rangeMinMouseMoveHandler);
-      document.removeEventListener('mouseup', rangeMinMouseUpHandler);
-    }
-
-    document.addEventListener('mouseup', rangeMinMouseUpHandler);
-  }
-
-  function getCoords(elem) {
-    var elCoords = elem.getBoundingClientRect();
-    return {
-      top: elCoords.top + pageYOffset,
-      left: elCoords.left + pageXOffset
-    };
   }
 
   return {
     listenToPriceRadio: function () {
-      rangeMin.addEventListener('mousedown', rangeMinMouseDownHandler);
-      rangeMax.addEventListener('mousedown', rangeMaxMouseDownHandler);
+      rangeBtnLeft.addEventListener('mouseup', changePriceRange);
+      rangeBtnRight.addEventListener('mouseup', changePriceRange);
     },
-    onFilterChange: function (/* callback*/) {
-      // filterCallback = callback;
+    onFilterChange: function (callback) {
+      filterCallback = callback;
+      filterCallback();
     }
   };
 })();
@@ -510,7 +447,6 @@ var initModule = (function (options) {
       });
 
       _productModule.addBtnClick(_cartModule.addToCard);
-
 
       catalogCardsElement.classList.remove('catalog__cards--load');
       catalogLoadElement.classList.add('visually-hidden');
