fc436795bb16ee472e09fceaae0b6e1d9d388fcb
ivana.begovic@yahoo.com <ivana.begovic@yahoo.com>
2018-10-11 00:37:46 +0300

    fixes

--- js/util.js
+++ js/util.js
@@ -1,15 +1,11 @@
 'use strict';
 
 var utilModule = (function () {
-  var catalogCardsElement = document.querySelector('.catalog__cards');
-  var catalogLoadElement = document.querySelector('.catalog__load');
 
   return {
     main: function () {
-      catalogCardsElement.classList.remove('catalog__cards--load');
-      catalogLoadElement.classList.add('visually-hidden');
     }
   };
 })();
 
-utilModule.main();
+window.utilModule = utilModule;
